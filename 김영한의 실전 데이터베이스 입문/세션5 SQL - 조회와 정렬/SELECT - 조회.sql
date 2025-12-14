417b22109baa653fcb0d3357ab9bd23ef42ba618
seowoo <dkr0210@naver.com>
2025-12-15 00:14:49 +0900

    김영한의 실전 데이터베이스 입문 - SELECT 조회

---
+++ 김영한의 실전 데이터베이스 입문/세션5 SQL - 조회와 정렬/SELECT - 조회.sql
@@ -0,0 +1,9 @@
+# customers 테이블의 모든 고객 정보 조회하기
+SELECT * FROM customers;
+
+# customers 테이블의 고객명, 이메일 정보 조회하기
+SELECT name, email FROM cutomers;
+
+# 별칭 사용(가독성)
+SELECT name AS 고객명, email AS 이메일 FROM customers;
+
