12652a6cae68661fd5c8e7a341d240ad8aff36da
seowoo <dkr0210@naver.com>
2026-03-15 18:48:09 +0900

    김영한의 실전 데이터베이스 기본 - 프로시저, 함수, 트리거 소개

---
+++ 김영한의 실전 데이터베이스 기본/섹션12 저장 프로시저, 함수, 트리거/프로시저, 함수, 트리거 - 소개.sql
@@ -0,0 +1,10 @@
+# 1. 저장 프로시저(Stored Procedure)
+# : 이름이 부여된 일련의 SQL 작업 묶음
+
+# 2. 저장 함수(Stored Function)
+# : 특정 계산을 수행하고 반드시 '하나의 값'을 반환하는 프로그램
+
+# 3. 트리거(Trigger)
+# : 특정 테이블에 특정 이벤트(Event)가 발생했을 때, 자동으로 실행되도록 약속된 프로그램
+# 이벤트(Event) : INSERT, UPDATE, DELETE 같은 데이터 변경 작업을 말함
+
